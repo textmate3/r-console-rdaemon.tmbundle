064f3d5fb3db494de8cb972f1e6ff6ab6fdea5fe
Shane Becker <veganstraightedge@gmail.com>
2026-05-31 23:42:37 -0700

    shebangs: ruby18/ruby20 -> ruby; drop 1.8-only flags (-K*, -rjcode)

--- Support/Rdaemon/help/savePlotAs.sh
+++ Support/Rdaemon/help/savePlotAs.sh
@@ -22,32 +22,32 @@ echo -ne "Enter a valid and non-existing file name without an extension" | perl 
 while [ "$TMD_returnCode" != 6 ]
 do
 	export DIA=$( cat "$MYDIR"/bin/icf.plist | "$DIALOG" -m "$MYDIR"/nibs/saveimage.nib )
-	TMD_returnCode=`cat <<-AS | ruby18 --
+	TMD_returnCode=`cat <<-AS | ruby --
     require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
     print OSX::PropertyList::load(%x{echo -en "$DIA"})["returnCode"]
     AS
     `
-	TMD_outfmtValue=`cat <<-AS | ruby18 --
+	TMD_outfmtValue=`cat <<-AS | ruby --
     require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
     print OSX::PropertyList::load(%x{echo -en "$DIA"})["outfmtValue"]
     AS
     `
-	TMD_filename=`cat <<-AS | ruby18 --
+	TMD_filename=`cat <<-AS | ruby --
     require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
     print OSX::PropertyList::load(%x{echo -en "$DIA"})["filename"].gsub(/^~/,ENV["HOME"])
     AS
     `
-	TMD_qualityValue=`cat <<-AS | ruby18 --
+	TMD_qualityValue=`cat <<-AS | ruby --
     require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
     print OSX::PropertyList::load(%x{echo -en "$DIA"})["qualityValue"]
     AS
     `
-	TMD_tiffcompValue=`cat <<-AS | ruby18 --
+	TMD_tiffcompValue=`cat <<-AS | ruby --
     require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
     print OSX::PropertyList::load(%x{echo -en "$DIA"})["tiffcompValue"]
     AS
     `
-	TMD_gray=`cat <<-AS | ruby18 --
+	TMD_gray=`cat <<-AS | ruby --
     require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
     print OSX::PropertyList::load(%x{echo -en "$DIA"})["gray"]
 AS
